9b7ef1c7aacfa9d3d12ec49837108bf6c99a7500
Nolan Kovacik <noltron000@gmail.com>
2021-03-02 15:07:49 -0500

    [add] styling and header component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,14 @@
-import logo from '../logo.svg';
-import './App.css';
+// import styles & other objects
+import './App.css'
+// import components
+import Header from './Header'
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<p>
-					This is a test.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
-	);
+		<>
+			<Header />
+		</>
+	)
 }
 
-export default App;
+export default App
