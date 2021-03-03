5bda9b5d8c22c44938297f9728328b999b433350
Nolan Kovacik <noltron000@gmail.com>
2021-03-03 14:53:20 -0500

    [add] basic redux functionality

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,7 +1,10 @@
 // import styles & other objects
+import {connect} from 'react-redux'
+import {activateSearch} from '../redux.js'
 import {Component} from 'react'
 import './Searchbar.css'
 
+/* CREATE BASE CLASS */
 class Searchbar extends Component {
 	constructor () {
 		super()
@@ -13,14 +16,19 @@ class Searchbar extends Component {
 	render () { return (<>
 		<form
 			onSubmit={async (event) => {
-				// don't allow the site to reload
+				// don't allow the site to reload.
 				event.preventDefault()
+
+				// add current query to redux history.
+				console.log(this.props.search)
+				const queries = [...this.props.search.queries, this.state.query]
+				this.props.activateSearch({'queries': queries})
+
 				// fetch some stuff from the API.
 				// its going to be a promise, so we'll await it later.
 				const promise = fetch(`https://hn.algolia.com/api/v1/search?query=${this.state.query}`)
 				const response = await promise
 				const data = await response.json()
-				console.log(data)
 
 				// check out this cool api fetch json
 				this.setState({
@@ -55,9 +63,27 @@ class Searchbar extends Component {
 
 			</div>
 		</form>
+
+		<p>JSON Results:</p>
 		<pre><code>{this.state.results}</code></pre>
-		</>
-	)}
+
+		<p>Search History:</p>
+		<pre><code>{JSON.stringify(this.props.search.queries)}</code></pre>
+	</>)}
 }
 
-export default Searchbar
+/* EXTEND INTO REDUX CONTAINER */
+const mapStateToProps = (state) => ({
+  search: state.search,
+})
+
+const mapDispatchToProps = {
+  activateSearch,
+}
+
+const SearchbarContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Searchbar)
+
+export default SearchbarContainer
---
+++ src/redux.js
@@ -0,0 +1,56 @@
+import {
+	combineReducers,
+	createStore,
+} from 'redux'
+
+
+/* ACTIONS */
+// check this out when the search bar is clicked.
+const activateSearch = (queries) => {
+	// create action object with a type label and its content.
+	const actionObject = {
+		'type': 'ACTIVATE_SEARCH',
+		'queries': queries,
+	}
+
+	// that's all we need.
+	return actionObject
+}
+
+
+/* REDUCERS */
+// bindle all possible actions for the search.
+const search = (state = {'queries': [ ]}, action) => {
+	// there's only one possible action type for queries.
+	if (action?.type === 'ACTIVATE_SEARCH') {
+		return action.queries
+	}
+
+	// if there's no matching labels, do nothing extra.
+	else {
+		return state
+	}
+}
+
+// bundle together all reducers.
+const reducers = combineReducers({search})
+
+/* STORE */
+// we're required to use a verbose f(x) behind the scenes.
+// verbose meaning, we use the 'function' keyword.
+function configureStore(initialState = {}) {
+	const store = createStore(reducers, initialState)
+	return store
+}
+
+// bundle up together the store
+const store = configureStore()
+
+/* EXPORT */
+export {
+	activateSearch,
+	search,
+	reducers,
+	configureStore,
+	store
+}
