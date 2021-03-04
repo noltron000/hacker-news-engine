d03617438f3ff6b26354c920d7900b32029ca721
Nolan Kovacik <noltron000@gmail.com>
2021-03-03 19:01:04 -0500

    [add] page-turning functionality

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,21 +1,12 @@
 // import styles & other objects
 import {connect} from 'react-redux'
+import {activateSearch} from '../redux.js'
 // import react components
 import {Component} from 'react'
 import Result from './Result'
 
 /* CREATE BASE CLASS */
 class Results extends Component {
-
-	// [x] "nbHits": 246802,
-	// [x] "page": 0,
-	// [x] "nbPages": 50,
-	// [x] "hitsPerPage": 20,
-	// [x] "exhaustiveNbHits": false,
-	// [ ] "query": "cool",
-	// [x] "params": "advancedSyntax=true&analytics=true&analyticsTags=backend&query=cool",
-	// [x] "processingTimeMS": 3
-
 	render () {return (
 		<section>
 			<h2>
@@ -40,13 +31,51 @@ class Results extends Component {
 				<input
 					type="button"
 					value="Previous Page"
-					disabled={this.props.search.results.page < this.props.search.results.nbPages}
+					disabled={this.props.search.results.page <= 0}
+					onClick={async () => {
+						// determine variables
+						const page = this.props.search.results.page - 1
+						const query = this.props.search.queries.slice(-1).pop()
+						const queries = this.props.search.queries
+
+						// fetch some stuff from the API using the proper page number.
+						// its going to be a promise, so we'll await them.
+						const promise = fetch(`https://hn.algolia.com/api/v1/search?query=${query}&page=${page}`)
+						const response = await promise
+						const results = await response.json()
+
+						// tell redux that we have new search data.
+						this.props.activateSearch({
+							// 'results' is fetched data for this query.
+							'results': results,
+							'queries': queries,
+						})
+					}}
 				/>
 				{" | "}
 				<input
 					type="button"
 					value="Next Page"
-					disabled={this.props.search.results.page > 0}
+					disabled={this.props.search.results.page >= this.props.search.results.nbPages}
+					onClick={async () => {
+						// determine variables
+						const page = this.props.search.results.page + 1
+						const query = this.props.search.queries.slice(-1).pop()
+						const queries = this.props.search.queries
+
+						// fetch some stuff from the API using the proper page number.
+						// its going to be a promise, so we'll await them.
+						const promise = fetch(`https://hn.algolia.com/api/v1/search?query=${query}&page=${page}`)
+						const response = await promise
+						const results = await response.json()
+
+						// tell redux that we have new search data.
+						this.props.activateSearch({
+							// 'results' is fetched data for this query.
+							'results': results,
+							'queries': queries,
+						})
+					}}
 				/>
 			</p>
 		</section>
@@ -64,8 +93,14 @@ const mapStateToProps = (state) => {
 	return props
 }
 
+const mapDispatchToProps = {
+	// "dispatch" bundled functions to incoming class context.
+  activateSearch,
+}
+
 const ResultsContainer = connect(
-  mapStateToProps
+  mapStateToProps,
+	mapDispatchToProps,
 )(Results)
 
 export default ResultsContainer
